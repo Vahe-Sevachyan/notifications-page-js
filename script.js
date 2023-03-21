0bed548f9cda77f188d0410451fdf3e494472fdb
Vahe Sevachyan <vscode13@gmail.com>
2023-03-20 23:33:34 -0700

    updated css, hover state, images

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let msgCounterNum = document.getElementById("msgCounter");
 const markAllRead = document.querySelector(".allRead-Header");
 const msgContainer = document.querySelectorAll(".msg-container");
 
-//this function changes the color of unread messages
+//this function changes the color of unread =>
+//messages by toggling the class "msg-containers"
 msgContainer.forEach((msg) => {
   msg.addEventListener("click", () => msg.classList.toggle("msg-containers"));
 });
-
-//this function decreases the msg count
+//this function decreases the msg count when messages are marked as read
 msgContainer.forEach((msg) => {
   msg.addEventListener("click", () => {
     if (msg.classList.contains("msg-containers")) {
@@ -17,7 +17,7 @@ msgContainer.forEach((msg) => {
     }
   });
 });
-
+//this function marks all messages as read and decreases msg counter to zero
 msgContainer.forEach((msg) => {
   markAllRead.addEventListener("click", () => {
     if (!msg.classList.contains("msg-containers")) {
