4ba6ab9883bbd948815016d8f40a335b88dda258
Vahe Sevachyan <vscode13@gmail.com>
2023-03-25 21:33:44 -0700

    finalized all functionality

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let msgCounterNum = document.getElementById("msgCounter");
 const markAllRead = document.querySelector(".allRead-Header");
 const msgContainer = document.querySelectorAll(".msg-container");
-
+const redDot = document.querySelectorAll(".red-dot");
 //this function changes the color of unread =>
 //messages by toggling the class "msg-containers"
 msgContainer.forEach((msg) => {
   msg.addEventListener("click", () => msg.classList.toggle("msg-containers"));
 });
+
+// console.log(redDot);
+// msgContainer.addEventListener("click", () => {
+//   redDot.forEach((dot) => {
+//     dot.classList.toggle("red-dots");
+//   });
+// });
+
+// redDot.classList.toggle("red-dots")
+
 //this function decreases the msg count when messages are marked as read
 msgContainer.forEach((msg) => {
   msg.addEventListener("click", () => {
@@ -17,6 +27,7 @@ msgContainer.forEach((msg) => {
     }
   });
 });
+
 //this function marks all messages as read and decreases msg counter to zero
 msgContainer.forEach((msg) => {
   markAllRead.addEventListener("click", () => {
